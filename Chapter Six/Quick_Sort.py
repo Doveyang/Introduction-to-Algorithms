d6ed34ef2a7ff8013cb588eac4b4763b0cfeecec
Nick Yang <yangfeichi@163.com>
2020-04-14 11:30:38 +0800

    Update on April 14th

--- Chapter Six/Quick_Sort.py
+++ Chapter Six/Quick_Sort.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+def partition(arr, p, r):
+    x = arr[r]
+    i = p - 1
+    for j in range(p, r):
+        if arr[j] <= x:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[r] = arr[r], arr[i+1]
+    return i + 1
